7a391ed14f45fef404a6b390a72a66f25cec9cca
Orphée Religieux <orphee@complex.ch>
2013-12-18 09:22:17 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'c51b240b008e06e6597b99cec31e30621579351cec384883d43556baa7b42546cace1d688d60bfb6c9b9ac7289ece829477ea403e358408b7074041c12d2c647'
